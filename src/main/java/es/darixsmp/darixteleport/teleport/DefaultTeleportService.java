a2624357fd5ac801a62e6dc99d2de2f4fffee2da
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2024-05-02 11:55:25 +0200

    Update default teleport service

--- src/main/java/es/darixsmp/darixteleport/teleport/DefaultTeleportService.java
+++ src/main/java/es/darixsmp/darixteleport/teleport/DefaultTeleportService.java
@@ -3,13 +3,18 @@ package es.darixsmp.darixteleport.teleport;
 import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
 import com.google.inject.Inject;
+import com.google.inject.name.Named;
 import es.darixsmp.darixteleport.DarixTeleport;
+import es.darixsmp.darixteleport.messenger.message.GetTeleportLocationRequest;
+import es.darixsmp.darixteleport.messenger.message.GetTeleportLocationResponse;
 import es.darixsmp.darixteleport.messenger.message.TeleportPlayerMessage;
 import es.darixsmp.darixteleportapi.pending.PendingTeleport;
 import es.darixsmp.darixteleportapi.pending.PendingTeleportService;
 import es.darixsmp.darixteleportapi.teleport.TeleportLocation;
 import es.darixsmp.darixteleportapi.teleport.TeleportService;
+import net.smoothplugins.smoothbase.configuration.Configuration;
 import net.smoothplugins.smoothbase.messenger.Messenger;
+import net.smoothplugins.smoothbase.messenger.Response;
 import net.smoothplugins.smoothbase.serializer.Serializer;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
@@ -27,6 +32,8 @@ public class DefaultTeleportService implements TeleportService {
     private PendingTeleportService pendingTeleportService;
     @Inject
     private DarixTeleport plugin;
+    @Inject @Named("config")
+    private Configuration config;
 
     @Override
     public void teleport(UUID playerUUID, TeleportLocation teleportLocation) {
@@ -51,7 +58,23 @@ public class DefaultTeleportService implements TeleportService {
     }
 
     @Override
-    public CompletableFuture<TeleportLocation> getTeleportLocation(UUID uuid) {
-        return null;
+    public CompletableFuture<TeleportLocation> getTeleportLocation(UUID playerUUID) {
+        CompletableFuture<TeleportLocation> future = new CompletableFuture<>();
+
+        GetTeleportLocationRequest request = new GetTeleportLocationRequest(playerUUID);
+        messenger.sendRequest(serializer.serialize(request), new Response() {
+            @Override
+            public void onSuccess(String channel, String json) {
+                GetTeleportLocationResponse response = serializer.deserialize(json, GetTeleportLocationResponse.class);
+                future.complete(response.getCurrentLocation());
+            }
+
+            @Override
+            public void onFail(String s) {
+                future.completeExceptionally(new Exception("No se ha podido obtener la ubicación del jugador"));
+            }
+        }, config.getLong("timeouts.get-teleport-location-request"));
+
+        return future;
     }
 }
