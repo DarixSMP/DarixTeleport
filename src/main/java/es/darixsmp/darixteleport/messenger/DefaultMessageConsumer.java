ea79d08c87c1239c9224a1f1c9fc5a04138f06b4
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2024-05-02 11:34:42 +0200

    Update DefaultMessageConsumer

--- src/main/java/es/darixsmp/darixteleport/messenger/DefaultMessageConsumer.java
+++ src/main/java/es/darixsmp/darixteleport/messenger/DefaultMessageConsumer.java
@@ -2,8 +2,12 @@ package es.darixsmp.darixteleport.messenger;
 
 import com.google.inject.Inject;
 import es.darixsmp.darixteleport.messenger.message.DefaultMessage;
+import es.darixsmp.darixteleport.messenger.message.TeleportPlayerMessage;
+import es.darixsmp.darixteleportapi.teleport.TeleportService;
 import net.smoothplugins.smoothbase.messenger.MessageConsumer;
 import net.smoothplugins.smoothbase.serializer.Serializer;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.UUID;
@@ -12,12 +16,23 @@ public class DefaultMessageConsumer implements MessageConsumer {
 
     @Inject
     private Serializer serializer;
+    @Inject
+    private TeleportService teleportService;
 
     @Override
     public void consume(String JSON, @Nullable UUID identifier) {
         DefaultMessage tempMessage = serializer.deserialize(JSON, DefaultMessage.class);
         switch (tempMessage.getType()) {
-            // TODO: Implement message handling
+            case TELEPORT_PLAYER -> {
+                TeleportPlayerMessage message = serializer.deserialize(JSON, TeleportPlayerMessage.class);
+            }
+        }
+    }
+
+    private void consumeTeleportPlayerMessage(TeleportPlayerMessage message) {
+        Player player = Bukkit.getPlayer(message.getPlayerUUID());
+        if (player != null) {
+            teleportService.teleport(message.getPlayerUUID(), message.getTarget());
         }
     }
 }
