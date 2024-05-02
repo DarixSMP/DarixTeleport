17659fdb15284bbd79f28d6dd813272422d85700
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2024-05-02 10:36:46 +0200

    Update DefaultMessageType.java

--- src/main/java/es/darixsmp/darixteleport/messenger/message/DefaultMessageType.java
+++ src/main/java/es/darixsmp/darixteleport/messenger/message/DefaultMessageType.java
@@ -1,8 +1,9 @@
 package es.darixsmp.darixteleport.messenger.message;
 
 public enum DefaultMessageType {
-    PLAYER_NOTIFICATION,
-    ACCEPTED_TPA_REQUEST,
-    GET_TELEPORT_LOCATION_REQUEST,
-    GET_TELEPORT_LOCATION_RESPONSE,
+    PLAYER_NOTIFICATION, // This is a notification message (e.g. "You have been teleported")
+    ACCEPTED_TPA_REQUEST, // This is a message that indicates that a TPA request has been accepted and the player can start the countdown
+    GET_TELEPORT_LOCATION_REQUEST, // This is a message that requests the location of a player
+    GET_TELEPORT_LOCATION_RESPONSE, // This is a message that contains the location of a player
+    TELEPORT_REQUEST, // This is a message that requests a teleport to the server that has the player
 }
