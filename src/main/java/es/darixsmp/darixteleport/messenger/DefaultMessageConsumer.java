149d5678f289d7255043a1d245cebbb09a5e105d
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2024-05-04 13:56:00 +0200

    Add messages

--- src/main/java/es/darixsmp/darixteleport/messenger/DefaultMessageConsumer.java
+++ src/main/java/es/darixsmp/darixteleport/messenger/DefaultMessageConsumer.java
@@ -1,13 +1,16 @@
 package es.darixsmp.darixteleport.messenger;
 
 import com.google.inject.Inject;
+import com.google.inject.name.Named;
 import es.darixsmp.darixteleport.DarixTeleport;
-import es.darixsmp.darixteleport.messenger.message.DefaultMessage;
-import es.darixsmp.darixteleport.messenger.message.GetTeleportLocationRequest;
-import es.darixsmp.darixteleport.messenger.message.GetTeleportLocationResponse;
-import es.darixsmp.darixteleport.messenger.message.TeleportPlayerMessage;
+import es.darixsmp.darixteleport.messenger.message.*;
+import es.darixsmp.darixteleportapi.countdown.CountdownService;
+import es.darixsmp.darixteleportapi.service.Destination;
 import es.darixsmp.darixteleportapi.teleport.TeleportLocation;
 import es.darixsmp.darixteleportapi.teleport.TeleportService;
+import es.darixsmp.darixteleportapi.user.User;
+import es.darixsmp.darixteleportapi.user.UserService;
+import net.smoothplugins.smoothbase.configuration.Configuration;
 import net.smoothplugins.smoothbase.messenger.MessageConsumer;
 import net.smoothplugins.smoothbase.messenger.Messenger;
 import net.smoothplugins.smoothbase.serializer.Serializer;
@@ -15,6 +18,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.HashMap;
 import java.util.UUID;
 
 public class DefaultMessageConsumer implements MessageConsumer {
@@ -25,6 +29,12 @@ public class DefaultMessageConsumer implements MessageConsumer {
     private TeleportService teleportService;
     @Inject
     private Messenger messenger;
+    @Inject
+    private CountdownService countdownService;
+    @Inject
+    private UserService userService;
+    @Inject @Named("messages")
+    private Configuration messages;
 
     @Override
     public void consume(String JSON, @Nullable UUID identifier) {
@@ -38,6 +48,16 @@ public class DefaultMessageConsumer implements MessageConsumer {
             case GET_TELEPORT_LOCATION_REQUEST -> {
                 GetTeleportLocationRequest message = serializer.deserialize(JSON, GetTeleportLocationRequest.class);
                 consumeGetTeleportLocationRequest(message, identifier);
+            }
+
+            case ACCEPTED_TPA -> {
+                AcceptedTPAMessage message = serializer.deserialize(JSON, AcceptedTPAMessage.class);
+                consumeAcceptedTPAMessage(message);
+            }
+
+            case PLAYER_MESSAGE -> {
+                PlayerMessage message = serializer.deserialize(JSON, PlayerMessage.class);
+                consumePlayerMessage(message);
             }
         }
     }
@@ -56,5 +76,32 @@ public class DefaultMessageConsumer implements MessageConsumer {
         TeleportLocation currentLocation = TeleportLocation.fromLocation(DarixTeleport.CURRENT_SERVER, player.getLocation());
         GetTeleportLocationResponse response = new GetTeleportLocationResponse(message.getPlayerUUID(), currentLocation);
         messenger.sendResponse(serializer.serialize(response), identifier);
+    }
+
+    private void consumeAcceptedTPAMessage(AcceptedTPAMessage message) {
+        Player player = Bukkit.getPlayer(message.getTarget());
+        if (player == null) return;
+
+        HashMap<String, String> placeholders = new HashMap<>();
+        placeholders.put("%target%", message.getSenderUsername());
+
+        player.sendMessage(messages.getComponent("commands.tpa.accepted", placeholders));
+
+        countdownService.startCountdown(player, () -> {
+            TeleportLocation currentLocation = TeleportLocation.fromLocation(DarixTeleport.CURRENT_SERVER, player.getLocation());
+            User user = userService.getUserByUUID(player.getUniqueId()).orElseThrow();
+            user.setLastLocation(currentLocation);
+            userService.update(user, Destination.CACHE_IF_PRESENT);
+
+            player.sendMessage(messages.getComponent("commands.tpa.teleporting"));
+            teleportService.teleport(player.getUniqueId(), message.getLocation());
+        });
+    }
+
+    private void consumePlayerMessage(PlayerMessage message) {
+        Player player = Bukkit.getPlayer(message.getPlayerUUID());
+        if (player == null) return;
+
+        message.getMessage().forEach(player::sendMessage);
     }
 }
